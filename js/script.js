2f301f7ff02fefdae43f7daba04a87968c616571
Joanna <66443179+ebookowa@users.noreply.github.com>
2020-07-18 15:57:08 +0200

    added active class to correct article

--- js/script.js
+++ js/script.js
@@ -46,6 +46,8 @@ const titleClickHandler = function (event) {
     console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
